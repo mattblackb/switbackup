eb3087fd84c4d3774b18e64482d37d98e05cd789
Matt Burton <matt@plain.black>
2021-09-11 12:39:37 +0100

    Initial commit

---
+++ BookMark__Not_App.swift
@@ -0,0 +1,20 @@
+//
+//  BookMark__Not_App.swift
+//  BookMark (Not)
+//
+//  Created by Matthew Burton on 10/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct BookMark__Not_App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            TestView(fileOnePath: "None", fileTwoPath: "none")
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ TestView.swift
@@ -0,0 +1,87 @@
+//
+//  TestView.swift
+//  BookMark (Not)
+//
+//  Created by Matthew Burton on 10/09/2021.
+//
+
+import Foundation
+import SwiftUI
+import SwiftExec
+
+
+struct TestView: View {
+
+    @State var fileOnePath: String
+    @State var fileTwoPath: String
+    @State private var showingAlert = false
+    @State private var message  = "Error!!!"
+    
+    var body: some View {
+        VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 10, content: {
+            Text("fileOnePath:" + fileOnePath)
+            Text("fileTwoPath:" + fileTwoPath)
+            Button("Choose Folder One") {
+                self.selectFolder(selfVar: "filePathOne")
+            
+            }
+            Button("Choose Folder Two") {
+                self.selectFolder(selfVar: "filePathTwo")
+            
+            }
+            Button(action: testExec) {
+                Label("Synchronise", systemImage: "plus")
+            }
+        }).padding(10)
+        .alert(isPresented: $showingAlert, content: {
+            Alert(title: Text("Important message"), message: Text(self.message), dismissButton: .default(Text("Got it!")))
+        })
+        .frame(width: 400, alignment: .center)
+       
+    }
+          
+
+    func testExec() {
+        if (self.fileOnePath != "None" &&  self.fileTwoPath != "None") {
+            do {
+                let result = try execBash("rsync -ahi \(self.fileOnePath)  \(self.fileTwoPath)")
+                print(result.stdout!)
+            } catch {
+                let error = error as! ExecError
+                    print(error.execResult)
+            }
+        }
+        else {
+            self.message = "Please choose both paths."
+            self.showingAlert = true
+        }
+       
+    }
+    func selectFolder(selfVar : String) {
+            var returnedString = ""
+            let folderChooserPoint = CGPoint(x: 0, y: 0)
+            let folderChooserSize = CGSize(width: 500, height: 600)
+            let folderChooserRectangle = CGRect(origin: folderChooserPoint, size: folderChooserSize)
+            let folderPicker = NSOpenPanel(contentRect: folderChooserRectangle, styleMask: .utilityWindow, backing: .buffered, defer: true)
+
+            folderPicker.canChooseDirectories = true
+            folderPicker.canChooseFiles = false
+            folderPicker.allowsMultipleSelection = false
+            folderPicker.canDownloadUbiquitousContents = true
+            folderPicker.canResolveUbiquitousConflicts = true
+
+            folderPicker.begin { response in
+                if response == .OK {
+                    let pickedFolders = folderPicker.urls
+                        returnedString = pickedFolders[0].absoluteString
+                        let replaced = returnedString.replacingOccurrences(of: "%20", with: #"\ "#)
+                        returnedString  = String(replaced.dropFirst(7))//Removes file:// from the front of the returned path string
+                        if(selfVar == "filePathOne") { //Probably better to return this filepath for easier use in the future.
+                            self.fileOnePath = returnedString
+                        } else {
+                            self.fileTwoPath = returnedString
+                        }
+                }
+            }
+        }
+}
